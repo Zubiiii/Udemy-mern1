fa7cb39fe612168d11706fa82edb58945cfb854c
Zubiiii <47924194+Zubiiii@users.noreply.github.com>
2019-07-13 14:05:15 -0400

    Update2
    Update2

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import React, {Component, Fragment} from 'react';
-import {BrowserRouter, Switch, Route} from 'react-router-dom'
-import './App.css';
-import Navbar from './components/Layout/Navbar';
-import Users from './components/Users/Users';
-import Search from './components/Users/Search';
-import Alert from './components/Layout/Alert'
-import axios from 'axios';
-
-
+import React, { Component, Fragment } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Layout/Navbar";
+import Users from "./components/Users/Users";
+import Search from "./components/Users/Search";
+import Alert from "./components/Layout/Alert";
+import About from "./components/Pages/About";
+import axios from "axios";
 
 class App extends Component {
-  
   state = {
     users: [],
     loading: false,
     alert: null
-  }
+  };
 
   /*async componentDidMount(){
     console.log(process.env.REACT_APP_GITHUB_CLIENT_SECRET);
@@ -27,40 +25,50 @@ class App extends Component {
  */
 
   searchUser = async text => {
-    this.setState({loading : true});
+    this.setState({ loading: true });
     const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=$
-      {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-      this.setState({users: res.data.items, loading:false, alert:null});  
-  }
-  
-  setAlert = (msg,type) => {
-    this.setState({alert:{msg,type}});
-    setTimeout(() => this.setState({alert:null}), 8000)
-  }
+      {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${
+        process.env.REACT_APP_GITHUB_CLIENT_SECRET
+      }`);
+    this.setState({ users: res.data.items, loading: false, alert: null });
+  };
 
-  clearUsers = () => this.setState({users: [], loading:false});
-  
-  render(){
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+    setTimeout(() => this.setState({ alert: null }), 8000);
+  };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  render() {
     return (
       <BrowserRouter>
-        <div className = 'App'>
-         <Navbar/>
-        <div className='container'>
-        <Alert alert={this.state.alert}/>
-        <Switch>
-          <Route exact path='/' render={ (props) => (
-            <Fragment>
-               <Search 
-                searchUser={this.searchUser} 
-                clearUsers={this.clearUsers} 
-                showClear={this.state.users.length > 0 ? true: false}
-                setAlert={this.setAlert}
-              /> 
-              <Users loading={this.state.loading} users={this.state.users}/>
-            </Fragment>
-          )} />
-        </Switch>
-        </div>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path="/"
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUser={this.searchUser}
+                      clearUsers={this.clearUsers}
+                      showClear={this.state.users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users
+                      loading={this.state.loading}
+                      users={this.state.users}
+                    />
+                  </Fragment>
+                )}
+              />
+              <Route exact path="/about" component={About} />
+            </Switch>
+          </div>
         </div>
       </BrowserRouter>
     );
@@ -68,9 +76,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-
-
-
---
+++ src/components/Pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from "react";
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>Hey welcome to the About Page</p>
+      <p>Routes Work!</p>
+    </Fragment>
+  );
+};
+
+export default About;
